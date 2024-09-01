562937cf69685ad78a1af5113ac798d16a56b44a
2000gmod <gmod20001@gmail.com>
2024-09-01 12:55:21 -0400

    Removed assembler, separated into another project

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "exec/vmachine.hpp"
 #include "loading/loading.hpp"
 #include "log/log.hpp"
-#include "assembler/parser.hpp"
 #include <cstdlib>
 #include <fstream>
 #include <sstream>
@@ -14,7 +13,7 @@ void MainError(const char* msg, int code = 1) {
     std::exit(code);
 }
 
-#define USE_DEBUG_ARGS 1
+#define USE_DEBUG_ARGS 0
 
 int main(int argc, char** argv) {
     args::ArgumentParser parser("");
@@ -25,11 +24,6 @@ int main(int argc, char** argv) {
     args::ValueFlag<unsigned long> localSize(executeFlags, "N", "Number of locals pre-allocated (in thousands).", {"xmL"}, 8);
     args::ValueFlag<std::string> entryPoint(executeFlags, "func", "Name of entry function (defaults to \"main\").", {'e', "entry"}, "main");
 
-    args::Group assemblyGroup(parser, "Assembler options", args::Group::Validators::AllOrNone);
-    args::Flag assemble(assemblyGroup, "", "Enable assembler (max. 1 input file).", {'a', "asm"});
-    args::ValueFlag<std::string> outputFile(assemblyGroup, "out", "Specify output file (if using assembler).", {'o', "output"});
-
-    args::Flag runFromAssembler(parser, "", "Run a single RVM assembly file directly.", {"rs"});
     args::Flag verbose(parser, "", "Verbose mode.", {'v', "verbose"});
     
     args::PositionalList<std::string> inputFiles(parser, "files", "Input files.");
@@ -60,7 +54,7 @@ int main(int argc, char** argv) {
                 "-v",
             };
 
-            parser.ParseArgs(args4);
+            parser.ParseArgs(args1);
         }
     }
     catch (const args::Help&) {
@@ -84,48 +78,24 @@ int main(int argc, char** argv) {
 
     if (inputFiles->size() == 0) MainError("Expected input file(s).");
 
-    if (runFromAssembler) {
-        if (inputFiles->size() != 1) {
-            MainError("Expected only a single input when using assembler.");
-        }
 
-        auto code = rvm::assembler::Parser::FromFile(inputFiles.Get()[0]);
+    std::vector<rvm::loading::GlobalDataUnit> code;
+    for (auto& inFile : inputFiles) {
+        std::ifstream inStream(inFile);
+        if (!inStream.is_open()) MainError("Could not open input file.");
 
-        rvm::exec::VirtualMachine vm(stackSize.Get() * 1024 * 1024 / 8, localSize.Get() * 1000);
-        vm.LoadBytecode(code);
-        vm.Run(entryPoint.Get());
-        return 0;
+        std::stringstream buf;
+        buf << inStream.rdbuf();
+
+        auto deserialized = rvm::loading::Deserialize(buf.str());
+
+        for (auto& unit : deserialized) code.push_back(unit);
     }
 
-    if (assemble) {
-        if (inputFiles->size() != 1) {
-            MainError("Expected only a single input when using assembler.");
-        }
+    rvm::exec::VirtualMachine vm(stackSize.Get() * 1024 * 1024 / 8, localSize.Get() * 1000);
+    vm.LoadBytecode(code);
+    vm.Run(entryPoint.Get());
 
-        auto code = rvm::assembler::Parser::FromFile(inputFiles.Get()[0]);
-        std::ofstream outStream(outputFile.Get());
-        if (!outStream.is_open()) MainError("Could not open output file.");
-        
-        auto serialized = rvm::loading::Serialize(code);
-        outStream << serialized;
-    }
-    else {
-        std::vector<rvm::loading::GlobalDataUnit> code;
-        for (auto& inFile : inputFiles) {
-            std::ifstream inStream(inFile);
-            if (!inStream.is_open()) MainError("Could not open input file.");
 
-            std::stringstream buf;
-            buf << inStream.rdbuf();
-
-            auto deserialized = rvm::loading::Deserialize(buf.str());
-
-            for (auto& unit : deserialized) code.push_back(unit);
-        }
-
-        rvm::exec::VirtualMachine vm(stackSize.Get() * 1024 * 1024 / 8, localSize.Get() * 1000);
-        vm.LoadBytecode(code);
-        vm.Run(entryPoint.Get());
-    }
     return 0;
 }
